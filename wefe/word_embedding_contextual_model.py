cd007216ab4b62c66c7d275a73f5d0e7a5f2c326
Humberto Rodrigues <humberto.rdrgs@gmail.com>
2022-05-12 12:28:15 -0400

    fixing

--- wefe/word_embedding_contextual_model.py
+++ wefe/word_embedding_contextual_model.py
@@ -23,7 +23,7 @@ class WordEmbeddingContextualModel(WordEmbeddingBaseModel):
     print(res)
     res = self.tokenizer(word,return_tensors="pt")
     start_idx = 0 if self.use_cls else 1
-    res.values.map(lambda x: x[None,0,start_idx:-1)
+    res.values.map(lambda x: x[None,0,start_idx:-1])
     print(res)
     
     # res["input_ids"] = res["input_ids"][None,0,start_idx:-1]
