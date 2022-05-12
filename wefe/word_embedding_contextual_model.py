c1b24b3d89df05ffeb0ff2b16cbd5d178be696f5
Humberto Rodrigues <humberto.rdrgs@gmail.com>
2022-05-12 12:23:35 -0400

    fixing

--- wefe/word_embedding_contextual_model.py
+++ wefe/word_embedding_contextual_model.py
@@ -8,25 +8,30 @@ class WordEmbeddingContextualModel(WordEmbeddingBaseModel):
       "sum": np.sum
   }
   def __init__(
-    self, tokenizer, hugging_face_pretrained_model, use_control_tokens=True, name = None, aggregation="mean"):
+    self, tokenizer, hugging_face_pretrained_model, use_cls=True, name = None, aggregation="mean"):
     if isinstance(hugging_face_pretrained_model,PreTrainedModel): 
       self.wv = hugging_face_pretrained_model
     if isinstance(tokenizer,PreTrainedTokenizerBase): 
       self.tokenizer = tokenizer
     
-    self.use_control_tokens = use_control_tokens
+    self.use_cls = use_cls
     self.name = name
     self.aggregation_method = self.aggregation_methods[aggregation]
     self.vocab_prefix = None
   
   def get_word_tokens(self, word):
+    print(res)
     res = self.tokenizer(word,return_tensors="pt")
+    start_idx = 0 if self.use_cls else 1
+    res.values.map(lambda x: x[None,0,start_idx:-1)
+    print(res)
     
-    if not self.use_control_tokens:
-      res["input_ids"] = res["input_ids"][None,0,1:-1]
-      res["token_type_ids"] = res["token_type_ids"][None,0,1:-1]
-      res["attention_mask"] = res["attention_mask"][None,0,1:-1]
+    # res["input_ids"] = res["input_ids"][None,0,start_idx:-1]
+    # res["token_type_ids"] = res["token_type_ids"][None,0,start_idx:-1]
+    # res["attention_mask"] = res["attention_mask"][None,0,start_idx:-1]
     return res
+    
+    
 
   def __getitem__(self, word):
     word_tokens = self.get_word_tokens(word)
